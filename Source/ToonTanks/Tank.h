b803c92ee7f2ca0c10f4a2e517e3ff875f45cd35
Jihoon Kim <jihoonerd@outlook.com>
2023-01-13 08:19:07 +0900

    Add ATANK class

---
+++ Source/ToonTanks/Tank.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Tank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+	ATank();
+	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+private:
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	USpringArmComponent* SpringArm;
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	UCameraComponent* CameraComp;
+
+	void Move(float Value);
+};
+
